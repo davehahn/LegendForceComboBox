6af984d727a229089302da4cb46ac59ef9563eba
Dave Hahn <angrysloth15@gmail.com>
2014-10-28 09:27:59 -0400

    small change to documentation in jQuery.LegendForceComboBox.js

--- jQuery.LegendForceComboBox.js
+++ jQuery.LegendForceComboBox.js
@@ -1,26 +1,39 @@
 /*   ##############   LegendForce Combo Box    ##################### */
 /*
   This plugin creates live text input from a standard text input.  The results are displayed in a drop down box
-  under the input field
+  under the input field similiar to a select field
+
+  Feel free to use this any way you feel and if it is something you like please throw a little credit my way
+  Originally created by Dave Hahn for use by Legend Boats of Canada
 */
 
 /*
 Options:
+
   resultsContainerClass: "the class applied to the container under the input" - DEFAULT = 'comboBoxResults'
+
+  minTextLength: "the minimum number of characters to be entered before the search function fires" - DEFAULT = 3
+
   idFieldName: "this is field name of the JSON search results that can be sent
                 back to the server to select the result" - DEFAULT = 'Id'
+
   textFieldName: "the name of the field of the JSON results which will be used
                   to display search results in the results drop box box" - DEFAULT = 'Name'
+
   searchFunction: "the function which queries the server. It MUST return a jQuery
                    Deferred object. The query string (input text) is supplied to the function"
+
   quickSelectHandler: "function that get fired when the user 'arrows down' the search results
                        container when the entry is highlighted. The result of the query for
                        this item is supplied to the function"
+
   selectHandler: "function is called when the item in the search results is selected, by either
                   hitting enter, or clicking in the entry. The value of the idFieldName,
                   value of the textFieldName, and the entire result object for that item are
                   supplied to the function"
+
   onClose: "callback fired when ever the results container is closed"
+
 */
 
 /*
